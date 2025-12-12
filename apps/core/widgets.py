4dfe247b41b60435ef3a3414d20738932acaa63a
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-12-11 22:11:40 -0300

    feat: Add CPForCNPJInput widget with Alpine.js interactivity and dedicated template

--- apps/core/widgets.py
+++ apps/core/widgets.py
@@ -1,4 +1,24 @@
 from django import forms
 
+
 class MoneyInput(forms.TextInput):
     template_name = 'money_input.html'
+
+
+class CPForCNPJInput(forms.TextInput):
+    """
+    mode:
+      - "cpf"  -> máscara/limite CPF
+      - "cnpj" -> máscara/limite CNPJ
+      - "both" -> alterna CPF até 11 dígitos e CNPJ acima
+    """
+    template_name = "cpf_or_cpnj_input.html"
+
+    def __init__(self, *args, mode: str = "both", **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mode = mode
+
+    def get_context(self, name, value, attrs):
+        ctx = super().get_context(name, value, attrs)
+        ctx["widget"]["doc_mode"] = self.mode
+        return ctx
