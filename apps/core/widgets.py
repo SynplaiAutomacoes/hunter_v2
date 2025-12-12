e60e269d9a018a7e796f07eb30b621b87f8c33b5
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-12-12 11:00:32 -0300

    feat: Add SelectInput and CalendarDateInput widgets with dedicated templates

--- apps/core/widgets.py
+++ apps/core/widgets.py
@@ -1,4 +1,5 @@
 from django import forms
+from typing import List, Tuple
 
 
 class MoneyInput(forms.TextInput):
@@ -42,3 +43,62 @@ class DurationInput(forms.TextInput):
 
 class EmailInput(forms.EmailInput):
     template_name = "widgets/email_input.html"
+
+
+# Não usei "forms.Select" por que não consegui estilizá-lo corretamente, portanto fiz está "gambiarra".
+class SelectInput(forms.TextInput):
+    template_name = "widgets/select_input.html"
+
+    def __init__(self, *args, choices: List[Tuple[str, str]] | None = None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.choices = choices or []
+
+    def get_context(self, name, value, attrs):
+        ctx = super().get_context(name, value, attrs)
+        ctx["widget"]["optgroups"] = self.optgroups(name, value, attrs)
+        return ctx
+
+    def optgroups(self, name, value, attrs=None):
+        if value is None:
+            value = []
+        if not isinstance(value, (list, tuple)):
+            value = [value]
+        str_values = {"" if v is None else str(v) for v in value}
+
+        groups = []
+        group_name = None
+        subgroup = []
+        group_index = 0
+
+        has_selected = False
+        for index, (opt_value, opt_label) in enumerate(self.choices):
+            opt_value_str = "" if opt_value is None else str(opt_value)
+            selected = (opt_value_str in str_values) and (not has_selected)
+            if selected:
+                has_selected = True
+
+            subgroup.append(
+                {
+                    "name": name,
+                    "value": opt_value_str,
+                    "label": opt_label,
+                    "selected": selected,
+                    "attrs": attrs or {}
+                }
+            )
+
+        groups.append((group_name, subgroup, group_index))
+        return groups
+
+    def value_from_datadict(self, data, files, name):
+        return data.get(name, "")
+
+
+class CalendarDateInput(forms.DateInput):
+    template_name = "widgets/calendar_date_input.html"
+
+    def __init__(self, *args, **kwargs):
+        # HTML date input (abre picker nativo no mobile/desktop quando suportado)
+        attrs = kwargs.setdefault("attrs", {})
+        attrs.setdefault("type", "date")
+        super().__init__(*args, **kwargs)
