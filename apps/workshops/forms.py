7d7ff4ac3f46cdefa623e1219cda97152426eb9a
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2026-01-05 00:13:15 -0300

    refactor: replace history.back() with reverse-resolved URL in cancel button
    - Update cancel button in workshop form to use `reverse("workshops:list")` for URL resolution.
    - Improve navigation consistency and maintainability with Django's URL resolver.

--- apps/workshops/forms.py
+++ apps/workshops/forms.py
@@ -2,7 +2,7 @@ from crispy_forms.layout import Submit, Layout, Div, HTML, Field
 from django.forms import ModelForm
 
 from crispy_forms.helper import FormHelper
-
+from django.urls import reverse
 
 from apps.workshops.models import Workshop
 from apps.core.widgets import TextInput, CPForCNPJInput, CheckboxInput
@@ -21,6 +21,8 @@ class WorkshopForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        cancel_url = reverse("workshops:list")
+
         self.helper = FormHelper()
         self.helper.layout = Layout(
             Div(
@@ -31,7 +33,7 @@ class WorkshopForm(ModelForm):
             ),
             HTML('<div class="divider"></div>'),
             Div(
-                HTML('<a href="javascript:history.back()" class="btn btn-ghost text-base-content/70">Cancelar</a>'),
+                HTML(f'<a href="{cancel_url}" class="btn btn-ghost text-base-content/70">Cancelar</a>'),
                 Submit("submit", "Salvar", css_class="btn btn-primary px-8"),
                 css_class="flex items-center justify-end gap-2",
             ),
