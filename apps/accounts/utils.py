f9f2a09b2f72189ddbd1ee7ec075cc8efefbaf15
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2026-01-07 17:42:38 -0300

    refactor: remove collaborator role creation logic from accounts utilities and views
    - Delete `get_or_create_collaborator_role` function from `utils.py`.
    - Remove `COLLABORATOR_ROLE_NAME` constant.
    - Update `SignUpFormView` to exclude collaborator role creation.

--- apps/accounts/utils.py
+++ apps/accounts/utils.py
@@ -6,7 +6,6 @@ from apps.accounts.models import Account, WorkshopRole
 
 
 DIRECTOR_ROLE_NAME = "Diretor"
-COLLABORATOR_ROLE_NAME = "Colaborador"
 
 
 def get_or_create_director_role(*, account: Account, with_all_permissions: bool = True) -> WorkshopRole:
@@ -21,18 +20,5 @@ def get_or_create_director_role(*, account: Account, with_all_permissions: bool 
 
     if with_all_permissions:
         role.permissions.set(Permission.objects.all())
-
-    return role
-
-
-def get_or_create_collaborator_role(*, account: Account) -> WorkshopRole:
-    role, _ = WorkshopRole.objects.get_or_create(
-        account=account,
-        name=COLLABORATOR_ROLE_NAME,
-        defaults={
-            "is_system": True,
-            "is_editable": True,
-        },
-    )
 
     return role
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 from apps.accounts.models import Account
-from apps.accounts.utils import get_or_create_collaborator_role, get_or_create_director_role
+from apps.accounts.utils import get_or_create_director_role
 
 from .forms import LoginForm, SignUpForm
 
@@ -35,7 +35,6 @@ class UserSignUpView(FormView):
             user.save(update_fields=["account"])
 
             get_or_create_director_role(account=account, with_all_permissions=True)
-            get_or_create_collaborator_role(account=account)
 
         return super().form_valid(form)
 
