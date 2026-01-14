1b82f165a0445116802ca3e631e1bcaf8b175cf7
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2026-01-14 12:16:25 -0300

    feat: Initial investigative questions creation

--- apps/quote/urls.py
+++ apps/quote/urls.py
@@ -1,3 +1,17 @@
+from django.urls import path
+
+from apps.quote.views.investigative_questions import (
+    InvestigativeQuestionListView,
+    InvestigativeQuestionCreateView,
+    InvestigativeQuestionUpdateView,
+    InvestigativeQuestionDeleteView,
+)
+
 app_name = "quote"
 
-urlpatterns = []
+urlpatterns = [
+    path("investigative-questions/", InvestigativeQuestionListView.as_view(), name="investigative_question_list"),
+    path("investigative-questions/create/", InvestigativeQuestionCreateView.as_view(), name="investigative_question_create"),
+    path("investigative-questions/<int:pk>/edit/", InvestigativeQuestionUpdateView.as_view(), name="investigative_question_update"),
+    path("investigative-questions/<int:pk>/delete/", InvestigativeQuestionDeleteView.as_view(), name="investigative_question_delete"),
+]
