a438d645533b1119965622246164f3a4a1b80e5b
Gabriel Galiza <briel.galiza@gmail.com>
2026-01-14 10:10:31 -0300

    feat: new Address model

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,4 +1,6 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from localflavor.br.models import BRPostalCodeField, BRStateField
 
 
 class TimeStampedModel(models.Model):
@@ -7,3 +9,13 @@ class TimeStampedModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class Address(models.Model):
+    cep = BRPostalCodeField(verbose_name="CEP")
+    logradouro = models.CharField(verbose_name="Logradouro", max_length=225)
+    numero = models.CharField(verbose_name="Número", max_length=8, validators=[MinValueValidator(0)])
+    complemento = models.CharField(verbose_name="Complemento", max_length=255, null=True, blank=True)
+    bairro = models.CharField(verbose_name="Bairro", max_length=20)
+    cidade = models.CharField(verbose_name="Cidade", max_length=20)
+    estado = BRStateField(verbose_name="Estado")
