373d9d2e55cc7e1bd0884a73bedf50533488025d
Gabriel Galiza <briel.galiza@gmail.com>
2026-01-15 12:36:41 -0300

    feat: create core app urls

--- config/urls.py
+++ config/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("catalog/", include("apps.catalog.urls")),
     path("suppliers/", include("apps.suppliers.urls")),
     path("quote/", include("apps.quote.urls")),
+    path("core/", include("apps.core.urls")),
 ]
