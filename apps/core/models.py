7e434927d24b5e85d2ff5e05c8cfe14e96eb58fc
Gabriel Galiza <briel.galiza@gmail.com>
2026-01-19 12:07:20 -0300

    feat: add verbose name to TimeStampedModel and add checklist to navbar

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class TimeStampedModel(models.Model):
-    criado_em = models.DateTimeField(auto_now_add=True)
-    atualizado_em = models.DateTimeField(auto_now=True)
+    criado_em = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
+    atualizado_em = models.DateTimeField(auto_now=True, verbose_name="Data de Atualização")
 
     class Meta:
         abstract = True
