e41341be3e960f17a8f3a9a307f3a056b458e208
Gabriel Galiza <briel.galiza@gmail.com>
2026-01-20 16:58:13 -0300

    feat: initial config

--- config/urls.py
+++ config/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path("collaborators/", include("apps.collaborators.urls")),
     path("catalog/", include("apps.catalog.urls")),
     path("quote/", include("apps.quote.urls")),
+    path("customer/", include("apps.customer.urls")),
 ]
