f09f188d7ace84c88b6cff5dc5780c307a907288
Gabriel Galiza <briel.galiza@gmail.com>
2026-01-21 11:57:37 -0300

    remove: vehicle js file

--- static/js/vehicle.js
+++
@@ -1,8 +0,0 @@
-document.body.addEventListener('htmx:afterSwap', function(evt) {
-    if (evt.detail.target.id === "vehicle-list") {
-        // Atualiza o TOTAL_FORMS com a quantidade real de .vehicle-item na tela
-        const totalForms = document.getElementById('id_vehicles-TOTAL_FORMS');
-        const currentForms = document.querySelectorAll('#vehicle-list .vehicle-item').length;
-        totalForms.value = currentForms;
-    }
-});
