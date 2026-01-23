e5594371195b79e2ef743fd60f36ba57f3060b71
Gabriel Galiza <briel.galiza@gmail.com>
2026-01-23 01:49:53 -0300

    feat: budget step 2 form

--- apps/budget/forms.py
+++ apps/budget/forms.py
@@ -122,10 +122,8 @@ class BudgetStep2Form(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         self.workshop = kwargs.pop("workshop", None)
+        self.request = kwargs.pop("request", None)
         super().__init__(*args, **kwargs)
-
-        if self.workshop:
-            self.fields["quotes"].queryset = self.fields["quotes"].queryset.filter(workshop=self.workshop)
 
         self.helper = FormHelper()
         self.helper.form_tag = False
@@ -133,31 +131,15 @@ class BudgetStep2Form(forms.ModelForm):
             Div(
                 HTML('<h3 class="text-2xl font-bold mb-2">Relato do Cliente</h3>'),
                 Div(
-                    Div(
-                        HTML('<h3 class="text-2xl font-bold mb-2">Veículo</h3>'),
-                        Div(
-                            Field("current_km", wrapper_class="col-span-12 lg:col-span-6"),
-                            Field("fuel_level", wrapper_class="col-span-12 lg:col-span-6"),
-                            css_class="grid grid-cols-1 lg:grid-cols-12 gap-4 items-start",
-                        ),
-                        css_class="mb-6 gap-4",
-                    ),
-                    css_class="col-span-12 lg:col-span-6",
+                    Field("problem_description", wrapper_class="col-span-12 lg:col-span-6"),
+                    css_class="col-span-12 lg:col-span-5 w-full",
                 ),
+                Div(css_class="col-span-12 lg:col-span-2"),
                 Div(
-                    Div(
-                        HTML('<h2 class="text-2xl font-bold mb-4 pb-2">Resumo</h2>'),
-                        # Cliente
-                        HTML('<h4 class="text-lg font-bold mb-2">Cliente</h4>'),
-                        Div(id="resumo-cliente", css_class="mb-6 overflow-x-auto"),
-
-                        # Veículo
-                        HTML('<h4 class="text-lg font-bold mb-2">Veículo</h4>'),
-                        Div(id="resumo-veiculo", css_class="overflow-x-auto"),
-                    ),
-                    css_class="col-span-12 lg:col-span-6",
+                    HTML("<span>Perguntas Investigativas</span>"),
+                    css_class="col-span-12 lg:col-span-5 w-full",
                 ),
                 Field("notes", wrapper_class="col-span-12 lg:col-span-12"),
-                css_class="grid grid-cols-1 lg:grid-cols-12 gap-38",
+                css_class="grid grid-cols-1 lg:grid-cols-12",
             ),
         )
