80f3ac9b5f96d62565fc59e6642ea5323ddbb318
gllugli <gabriel.lugli@gmail.com>
2026-02-08 17:21:27 -0300

    feat(workorders): add workorder status badge property for improved status display

--- apps/workorder/models.py
+++ apps/workorder/models.py
@@ -15,6 +15,22 @@ class WorkOrder(TimeStampedModel):
     workshop = models.ForeignKey( "workshops.Workshop", on_delete=models.CASCADE, related_name="workorders")
     budget = models.ForeignKey("budget.Budget", on_delete=models.CASCADE, related_name="workorders", help_text="Orçamento Aprovado vinculado à esta O.S.")
     status = models.CharField(verbose_name="Status", max_length=20, choices=WorkOrderStatus.choices, default=WorkOrderStatus.DRAFT)
+    
+    @property
+    def workorder_status_badge(self):
+        status_color = {
+            WorkOrderStatus.DRAFT: "badge-soft badge-ghost",
+            WorkOrderStatus.APPROVED: "badge-success",
+            # BudgetStatus.WAITING_DIAGNOSIS: "badge-soft badge-warning",
+            # BudgetStatus.WAITING_ITEMS: "badge-soft badge-warning",
+            # BudgetStatus.WAITING_PRICING: "badge-soft badge-info",
+            # BudgetStatus.WAITING_REVIEW: "badge-soft badge-info",
+            # BudgetStatus.APPROVED: "badge-success",
+            # BudgetStatus.REJECTED: "badge-error",
+            # BudgetStatus.CANCELLED: "badge-soft badge-error",
+        }
+
+        return {"text": WorkOrderStatus(self.status).label, "class": status_color.get(self.status, "badge-ghost")}
 
     class Meta:
         verbose_name = "Ordem de Serviço"
