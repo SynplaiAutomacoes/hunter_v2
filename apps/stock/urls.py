d90061b9d3d74bffd2a49388d24f980bc1c8f17e
Gabriel Galiza <briel.galiza@gmail.com>
2026-02-09 01:55:20 -0300

    feat: add alert page

--- apps/stock/urls.py
+++ apps/stock/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "stock"
 
 urlpatterns = [
+    path('alerts/', views.StockAlertsListView.as_view(), name='alerts'),
 ]
--- apps/stock/views.py
+++ apps/stock/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib import messages
+from django.db import transaction
+from django.db.models import F, ExpressionWrapper, IntegerField
+from .models import StockProduct, StockMovement
+from ..workshops.mixin import WorkshopScopedMixin
 
-# Create your views here.
+
+class StockAlertsListView(LoginRequiredMixin, WorkshopScopedMixin, ListView):
+    model = StockProduct
+    template_name = "stock/alerts.html"
+    context_object_name = "alerts"
+    workshop_permission_codename = "view_stockproduct"
+
+    def get_queryset(self):
+        return StockProduct.objects.filter(workshop=self.workshop, current_quantity__lte=F("minimum_quantity")).select_related("product")
