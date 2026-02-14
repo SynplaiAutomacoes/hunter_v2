d4424f985de0a06083e600efd22af35307001cc5
Vinicius <viniciusflugli@gmail.com>
2026-02-14 13:22:50 -0300

    feat(settings): add Webmania API configuration with environment variable support

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,9 @@ PERF_LOGGING_ENABLED = os.getenv("PERF_LOGGING_ENABLED", "0").lower() in ("1", "
 PERF_LOG_QUERIES = os.getenv("PERF_LOG_QUERIES", "0").lower() in ("1", "true", "yes")
 PERF_LOG_MIN_MS = int(os.getenv("PERF_LOG_MIN_MS", "300"))
 
+WEBMANIA_BASE_URL = "https://api.webmania.com.br/2/"
+WEBMANIA_AMBIENT = os.getenv("WEBMANIA_AMBIENT", "2")
+
 SUPERSIGN_BASE_URL = os.getenv("SUPERSIGN_BASE_URL", "https://api.sign.supersign.com.br")
 SUPERSIGN_ACCOUNT_ID = os.getenv("SUPERSIGN_ACCOUNT_ID", "")
 SUPERSIGN_API_KEY = os.getenv("SUPERSIGN_API_KEY", "")
