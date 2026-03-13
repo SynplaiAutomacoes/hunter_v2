aca1740e19b5c7d83447dbf90b5a39787eaa98b2
Vinícius <viniciusflugli@gmail.com>
2026-03-13 19:27:15 -0300

    feat: add due_date field to workorderpaymentmethod in migration

--- apps/workorder/migrations/0010_workorderpaymentmethod_due_date.py
+++ apps/workorder/migrations/0010_workorderpaymentmethod_due_date.py
@@ -1,16 +1,9 @@
-import django.utils.timezone
-from django.db import migrations, models
+from django.db import migrations
 
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("workorder", "0009_workorder_signature_external_id_and_more"),
+        ("workorder", "0011_workorderpaymentmethod_due_date"),
     ]
 
-    operations = [
-        migrations.AddField(
-            model_name="workorderpaymentmethod",
-            name="due_date",
-            field=models.DateField(default=django.utils.timezone.localdate, verbose_name="Vencimento"),
-        ),
-    ]
+    operations = []
