a7d3105723f1e51151bb2d0dbc7dcc886def0674
Vinícius <viniciusflugli@gmail.com>
2026-03-18 14:55:50 -0300

    feat: add workshop logo upload functionality and display in PDF

--- config/settings.py
+++ config/settings.py
@@ -209,6 +209,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Tailwind
 # TODO: Optimize for production https://github.com/django-commons/django-tailwind-cli?tab=readme-ov-file#production-settings
