3155a8af772653de173f8d5e81d5d6927a47af28
Gabriel Galiza <briel.galiza@gmail.com>
2026-04-19 17:32:17 -0300

    feat: add new fields collaborator and supplier, remove dre topic

--- apps/finance/models/financial_movement.py
+++ apps/finance/models/financial_movement.py
@@ -1,3 +1,5 @@
+from tokenize import blank_re
+
 from django.db import models
 
 from apps.core.models import TimeStampedModel
@@ -34,7 +36,9 @@ class FinancialMovement(TimeStampedModel):
     workorder_payment = models.ForeignKey("workorder.WorkOrderPaymentMethod", on_delete=models.CASCADE, null=True, blank=True, related_name="financial_movements")
 
     # Origem
-    source = models.ForeignKey(to="sources.Source", verbose_name="Origem", on_delete=models.PROTECT)
+    source = models.ForeignKey(to="sources.Source", verbose_name="Origem", null=True, blank=True, on_delete=models.PROTECT)
+    collaborator = models.ForeignKey("collaborators.WorkshopCollaborator", verbose_name="Colaborador", on_delete=models.SET_NULL, related_name="financial_movements", null=True, blank=True)
+    supplier = models.ForeignKey("suppliers.Supplier",  verbose_name="Fornecedor", on_delete=models.SET_NULL, null=True, blank=True, related_name="financial_movements")
 
     # Itens
     description = models.CharField(verbose_name="Descrição dos Itens", max_length=255, blank=True, null=True)
@@ -44,7 +48,6 @@ class FinancialMovement(TimeStampedModel):
     direction = models.CharField(max_length=15, verbose_name="Tipo", choices=MovementDirection.choices, default=MovementDirection.DEBIT, blank=True, null=True)
     payment_method = models.ForeignKey(PaymentMethod, verbose_name="Forma de Pagamento", on_delete=models.PROTECT, blank=True, null=True)
     nf_number = models.CharField(max_length=50, verbose_name="Número da NF", blank=True, null=True)
-    dre_topic = models.CharField(max_length=50, verbose_name="Tópico DRE", choices=DreTopic.choices, blank=True, null=True)
     amount = MoneyField(verbose_name="Valor", max_digits=14, decimal_places=2, default=0, null=True)
     due_date = models.DateField(verbose_name="Data de Vencimento", blank=True, null=True)
     is_paid = models.BooleanField(verbose_name="Pago", default=False)
