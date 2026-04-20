6c949f0988aab751714455ecdb680b72463a2e41
Gabriel Galiza <briel.galiza@gmail.com>
2026-04-20 14:36:18 -0300

    fix: approve budget error

--- apps/budget/views/workflow_views.py
+++ apps/budget/views/workflow_views.py
@@ -710,7 +710,7 @@ class UpdateBudgetStatusView(LoginRequiredMixin, WorkshopScopedMixin, View):
                 return JsonResponse({"success": False, "error": error_message}, status=400)
             except Exception as e:
                 print(f"E: {e}")
-                error_message = "Erro interno ao processar aprovação automática de estoque."
+                error_message = "Erro interno ao processar aprovação do orçamento."
                 messages.error(request, error_message)
                 return JsonResponse({"success": False, "error": error_message}, status=500)
 
