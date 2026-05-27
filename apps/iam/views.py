4d6631dcfe2450559f43d297d24400a98b202b80
Gabriel Galiza <briel.galiza@gmail.com>
2026-05-27 13:28:01 -0300

    fix: access control and permissions visibility for collaborators

--- apps/iam/views.py
+++ apps/iam/views.py
@@ -4,15 +4,15 @@ from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
-from apps.accounts.mixins import AccountOwnerRequiredMixin
 from apps.core.tables import TableActionDefaults
 from apps.core.templatetags.table_tags import TableColumn
 from apps.core.views import HtmxDeleteResponseMixin, HtmxTemplateResponseMixin
 from apps.iam.forms import WorkshopRoleForm
 from apps.iam.models import WorkshopRole
+from apps.workshops.mixin import WorkshopScopedMixin
 
 
-class WorkshopRoleListView(AccountOwnerRequiredMixin, HtmxTemplateResponseMixin, ListView):
+class WorkshopRoleListView(WorkshopScopedMixin, HtmxTemplateResponseMixin, ListView):
     model = WorkshopRole
     template_name = "iam/role_list.html"
     context_object_name = "roles"
@@ -20,7 +20,7 @@ class WorkshopRoleListView(AccountOwnerRequiredMixin, HtmxTemplateResponseMixin,
     htmx_template_name = "iam/partials/role_table.html"
 
     def get_queryset(self):
-        return super().get_queryset().filter(account=self.request.user.account).order_by("-criado_em")
+        return ListView.get_queryset(self).filter(account=self.request.user.account).order_by("-criado_em")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -48,7 +48,7 @@ class WorkshopRoleListView(AccountOwnerRequiredMixin, HtmxTemplateResponseMixin,
         return context
 
 
-class WorkshopRoleCreateView(AccountOwnerRequiredMixin, CreateView):
+class WorkshopRoleCreateView(WorkshopScopedMixin, CreateView):
     model = WorkshopRole
     form_class = WorkshopRoleForm
     template_name = "iam/role_create.html"
@@ -59,14 +59,14 @@ class WorkshopRoleCreateView(AccountOwnerRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-class WorkshopRoleUpdateView(AccountOwnerRequiredMixin, UpdateView):
+class WorkshopRoleUpdateView(WorkshopScopedMixin, UpdateView):
     model = WorkshopRole
     form_class = WorkshopRoleForm
     template_name = "iam/role_update.html"
     success_url = reverse_lazy("iam:role_list")
 
     def get_queryset(self):
-        return super().get_queryset().filter(account=self.request.user.account)
+        return UpdateView.get_queryset(self).filter(account=self.request.user.account)
 
     def dispatch(self, request, *args, **kwargs):
         response = super().dispatch(request, *args, **kwargs)
@@ -75,7 +75,7 @@ class WorkshopRoleUpdateView(AccountOwnerRequiredMixin, UpdateView):
         return response
 
 
-class WorkshopRoleDeleteView(AccountOwnerRequiredMixin, HtmxDeleteResponseMixin, DeleteView):
+class WorkshopRoleDeleteView(WorkshopScopedMixin, HtmxDeleteResponseMixin, DeleteView):
     model = WorkshopRole
     success_url = reverse_lazy("iam:role_list")
 
@@ -83,7 +83,7 @@ class WorkshopRoleDeleteView(AccountOwnerRequiredMixin, HtmxDeleteResponseMixin,
     htmx_trigger = "roles-table-refresh"
 
     def get_queryset(self):
-        return super().get_queryset().filter(account=self.request.user.account)
+        return DeleteView.get_queryset(self).filter(account=self.request.user.account)
 
     def dispatch(self, request, *args, **kwargs):
         response = super().dispatch(request, *args, **kwargs)
