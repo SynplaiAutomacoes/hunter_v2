a488dd819e2b131c5ec01fa683a23c062db9abfd
gllugli <gabriel.lugli@gmail.com>
2026-08-09 03:07:45 -0300

    feat(fiscal): integrate NFe flows on historical domain

---
+++ apps/finance/migrations/0091_merge_historical_fiscal_and_revert.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("finance", "0046_revert_staging_merge"),
+        ("finance", "0086_merge_20260801_1957"),
+    ]
+
+    operations = []
