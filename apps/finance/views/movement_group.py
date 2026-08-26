49b3fb9469d462d681254c16c16c59af4f9597ea
Enzo Dourado <enzodourado.cripto@gmail.com>
2026-08-26 10:51:20 -0300

    feat(finance): aplica desconto em agrupamentos

--- apps/finance/views/movement_group.py
+++ apps/finance/views/movement_group.py
@@ -101,7 +101,6 @@ class GroupMovementWizardView(LoginRequiredMixin, WorkshopScopedMixin, View):
 
                     group.save()
 
-                    total_amount = Decimal("0.00")
                     first_direction = FinancialMovement.MovementDirection.DEBIT
                     
                     if fm_ids:
@@ -110,7 +109,6 @@ class GroupMovementWizardView(LoginRequiredMixin, WorkshopScopedMixin, View):
                             first_direction = first_mv.direction
                         for mv in fms:
                             mv.movement_group = group
-                            total_amount += Decimal(str(mv.amount.amount))
                             mv.save()
                     
                     if pm_ids:
@@ -118,7 +116,6 @@ class GroupMovementWizardView(LoginRequiredMixin, WorkshopScopedMixin, View):
                             first_direction = FinancialMovement.MovementDirection.CREDIT
                         for pm in pms:
                             pm.movement_group = group
-                            total_amount += Decimal(str(pm.total_paid.amount))
                             pm.save()
 
                     payment_method = form.cleaned_data["payment_method"]
@@ -136,6 +133,7 @@ class GroupMovementWizardView(LoginRequiredMixin, WorkshopScopedMixin, View):
                             financial_observation=group.description,
                             due_date=installment.due_date,
                             amount=installment.amount,
+                            gross_amount=installment.amount,
                             direction=first_direction,
                             payment_method=payment_method,
                             supplier_id=group.supplier_id,
