6b33bdb8cbc5b20223562e8c64a51fc0c23b781e
Gabriel Galiza <briel.galiza@gmail.com>
2026-08-25 23:09:34 -0300

    feat(catalog): make OS clickable and add Cliente/Veiculo column in product movements

--- apps/catalog/views/products.py
+++ apps/catalog/views/products.py
@@ -189,7 +189,10 @@ class ProductUpdateView(LoginRequiredMixin, WorkshopScopedMixin, UpdateView):
 
         context["stock_obj"] = stock_obj
         context["movements"] = StockMovement.objects.filter(stock_product=stock_obj).select_related(
-            "workorder__budget"
+            "workorder__budget",
+            "workorder__budget__customer",
+            "workorder__budget__vehicle",
+            "transcation_by",
         ).order_by("-criado_em")
 
         budget_items = BudgetItem.objects.filter(product=product, workshop=self.workshop).select_related("budget", "budget__customer", "budget__vehicle")
