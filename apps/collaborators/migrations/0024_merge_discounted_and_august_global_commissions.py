058f9d1148a0dd56d5001583bad24646731e9e3d
Vinícius <viniciusflugli@gmail.com>
2026-09-02 14:21:47 -0300

    fix(migrations): restaurar 0022_recalculate e 0024_merge compativeis com banco de staging

--- apps/collaborators/migrations/0024_merge_discounted_and_august_global_commissions.py
+++ apps/collaborators/migrations/0024_merge_discounted_and_august_global_commissions.py
@@ -5,7 +5,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("collaborators", "0025_recalculate_discounted_global_commissions"),
+        ("collaborators", "0022_recalculate_discounted_global_commissions"),
         ("collaborators", "0023_regenerate_august_2026_global_commissions_v2"),
     ]
 
